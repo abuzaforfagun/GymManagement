1893ada5d9c690f63b3b75613048fd29ccdb3797
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-02 01:27:02 +0600

    add dto for member creation and update

--- Backend/GymManagement.Domain/Models/Presistance/MemberResourceForSave.cs
+++ Backend/GymManagement.Domain/Models/Presistance/MemberResourceForSave.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace GymManagement.Domain.Models.Presistance
 {
-    public class MemberDto
+    public class MemberResourceForSave
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Mobile { get; set; }
         public string Address { get; set; }
         public string ImageUrl { get; set; }
         public DateTime JoiningDate { get; set; }
-        public DateTime? RessignDate { get; set; }
     }
 }
---
+++ Backend/GymManagement.Domain/Models/Presistance/MemberResourceForUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagement.Domain.Models.Presistance
+{
+    public class MemberResourceForUpdate
+    {
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
