a7f785481bdd25be1da430d15144aa226a3e2678
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-01 15:44:51 +0600

    intial migrations

---
+++ Backend/GymManagement.Domain/Migrations/20180901084737_AddTblMember.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymManagement.Domain.Migrations
+{
+    public partial class AddTblMember : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 150, nullable: true),
+                    Mobile = table.Column<string>(maxLength: 20, nullable: true),
+                    Address = table.Column<string>(maxLength: 250, nullable: true),
+                    ImageUrl = table.Column<string>(maxLength: 500, nullable: true),
+                    JoiningDate = table.Column<DateTime>(nullable: false),
+                    RessignDate = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
