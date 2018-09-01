5d47bd776cf3453926a7864dac720bccb20b5140
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-01 13:33:57 +0600

    do dependency injection and db connection

--- Backend/GymManagement/Startup.cs
+++ Backend/GymManagement/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GymManagement.Domain.Models;
+using GymManagement.Domain.Models.Presistance;
+using GymManagement.Domain.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,7 +29,12 @@ namespace GymManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<IMember, Member>();
+            services.AddScoped<IMemberRepository, MemberRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("GymDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
