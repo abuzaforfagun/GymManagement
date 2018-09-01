3eda27170ffe3a97d35cd807dab967e70148417a
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-01 15:44:39 +0600

    add member dto

---
+++ Backend/GymManagement.Domain/Models/Presistance/MemberDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagement.Domain.Models.Presistance
+{
+    public class MemberDto
+    {
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public DateTime? RessignDate { get; set; }
+    }
+}
