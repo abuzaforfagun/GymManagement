627c5031687ad3b1b97576608b9ee27e9c7de4bb
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-01 13:10:29 +0600

    add db context

---
+++ Backend/GymManagement.Domain/Models/Presistance/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GymManagement.Domain.Models.Presistance
+{
+    public class AppDbContext : DbContext
+    {
+        private IConfiguration Configuration;
+        public AppDbContext(DbContextOptions<AppDbContext> options):
+            base(options)
+        {
+                
+        }
+
+        protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("GymDb"));
+            }
+        }
+    }
+}
