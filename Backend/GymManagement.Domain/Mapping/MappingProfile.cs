779be01beb21dfac1d12cad743fbb2490da8cc84
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-09-02 01:27:45 +0600

    add mapping of automapper

---
+++ Backend/GymManagement.Domain/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GymManagement.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GymManagement.Domain.Models.Presistance;
+
+namespace GymManagement.Domain.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Member, MemberResourceForUpdate>().ReverseMap()
+                .ForMember(m => m.JoiningDate, config => config.Ignore())
+                .ForMember(m => m.RessignDate, config => config.Ignore());
+            CreateMap<Member, MemberResourceForSave>().ReverseMap();
+
+        }
+    }
+}
