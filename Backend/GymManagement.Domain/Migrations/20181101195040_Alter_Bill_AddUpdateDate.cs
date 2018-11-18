73c711b1c0286d134d3e27c15187fddc078aa878
Fagun <mdabuzaforfagun@gmail.com>
2018-11-19 02:02:13 +0600

    add missing migrations

---
+++ Backend/GymManagement.Domain/Migrations/20181101195040_Alter_Bill_AddUpdateDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymManagement.Domain.Migrations
+{
+    public partial class Alter_Bill_AddUpdateDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Bill",
+                nullable: false,
+                defaultValue: DateTime.Now);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Bill");
+        }
+    }
+}
